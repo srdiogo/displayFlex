92bf3a87770fc5d6bf89806374f93febb55b15a4
Diogo Medeiros <sr.diogo@gmail.com>
2021-07-02 15:08:32 -0300

    exercicios js

---
+++ main.js
@@ -0,0 +1,33 @@
+const isPar = (x) => {
+  if (typeof(x) !== 'number') {return "O argumento não é um número"};
+  return x % 2 === 0;
+  }
+console.log(isPar('2'));
+
+const div = (x, y) => {
+    if (typeof(x||y) !== 'number') {return "O argumento não é um número"};
+    if (y === 0) {return "não pode dividir por 0"};
+    return [parseInt(x / y), x % y];
+};
+console.log(div(40,6));
+
+const potencia = (x, y) => {
+    if (typeof(x||y) !== 'number') {return "O argumento não é um número"};
+return x ** y;
+};
+console.log(potencia(9,4));
+
+const torta = (x, y) => {
+    if (typeof(x||y) !== 'number') {return "O argumento não é um número"};
+    for (n=1; n<=x; n++){
+        if(n % y !== 0) console.log(n);
+        else console.log("torta");
+    }; 
+};
+torta(10,3);
+
+const quantasPalavras = (frase) => {
+ palavras = frase.split(" ");
+ return palavras.length;
+};
+console.log(quantasPalavras("albatroz sentado na calçada"));
